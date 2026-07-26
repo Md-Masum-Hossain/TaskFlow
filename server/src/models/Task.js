10b55b342290757b73abf4b952aa1986e1acd80e
Md Masum Hossain <mdmasumhossain1712@gmail.com>
2026-07-26 23:49:03 +0600

    feat(task): add task model

--- server/src/models/Task.js
+++ server/src/models/Task.js
@@ -1,5 +1,33 @@
 import mongoose from 'mongoose';
 
-const taskSchema = new mongoose.Schema({}, { timestamps: true });
+const taskSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		description: {
+			type: String,
+			trim: true,
+		},
+		status: {
+			type: String,
+			enum: ['todo', 'in-progress', 'completed'],
+			default: 'todo',
+		},
+		priority: {
+			type: String,
+			enum: ['low', 'medium', 'high'],
+			default: 'medium',
+		},
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
 
 export default mongoose.model('Task', taskSchema);
