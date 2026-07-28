e025867a0a37080d2cc5e11ee2784fb640e87234
Md Masum Hossain <mdmasumhossain1712@gmail.com>
2026-07-28 20:50:00 +0600

    feat(task): add RTK Query task API endpoints

--- client/src/features/tasks/api/tasksApi.js
+++ client/src/features/tasks/api/tasksApi.js
@@ -1,1 +1,42 @@
-export const tasksApi = null;
+import { updateTask } from '../../../../../server/src/controllers/task.controller';
+import baseApi from '../../../app/api/baseApi';
+
+export const tasksApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getTasks: builder.query({
+            query: () => '/tasks',
+            providesTags: ['Tasks']
+        }),
+        createTask: builder.mutation({
+            query: (taskData) => ({
+                url: '/tasks',
+                method: 'POST',
+                body: taskData
+            }),
+            invalidatesTags: ['Tasks']
+        }),
+        updateTask: builder.mutation({
+            query: ({ id, ...taskData }) => ({
+                url: `/tasks/${id}`,
+                method: 'PUT',
+                body: taskData
+            }),
+            invalidatesTags: ['Tasks']
+        }),
+        deleteTask: builder.mutation({
+            query: (id) => ({
+                url: `/tasks/${id}`,
+                method: 'DELETE'
+            }),
+            invalidatesTags: ['Tasks']
+        })
+    }),
+});
+
+
+export const {
+    useGetTasksQuery,
+    useCreateTaskMutation,
+    useUpdateTaskMutation,
+    useDeleteTaskMutation
+} = tasksApi;
